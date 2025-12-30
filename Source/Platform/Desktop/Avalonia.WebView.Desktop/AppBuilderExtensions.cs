8a69c2e38fee4f567671b0fea0badeebe0bc49b9
ck <ck@qq.com>
2025-12-30 16:26:09 +0800

    remove linux builder

--- Source/Platform/Desktop/Avalonia.WebView.Desktop/AppBuilderExtensions.cs
+++ Source/Platform/Desktop/Avalonia.WebView.Desktop/AppBuilderExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System.Runtime.InteropServices;
+using System.Runtime.InteropServices;
 
 namespace Avalonia.WebView.Desktop;
 
@@ -9,8 +9,8 @@ public static class AppBuilderExtensions
 #if NETSTANDARD
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             builder.UseWindowWebView();
-        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            builder.UseLinuxWebView(isWslDevelop);
+        //else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        //    builder.UseLinuxWebView(isWslDevelop);
         if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             builder.UseMacCatalystWebView();
 #endif
